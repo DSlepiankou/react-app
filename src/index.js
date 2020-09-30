6908eec99e71b6cf10a1d93842403e6ec1ed6fcf
Slepiankou Dzmitry <Slepenkov95@mail.ru>
2020-09-30 22:55:25 +0300

    Add simple JSX components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("234");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ToDoList = () => {
+  return(
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  );  
+};
+
+const AppHeader = () => {
+  return <h1> My ToDo List</h1>;
+};
+
+const SearchPanel = () => {
+  return  <input placeholder="search"/>;
+}
+
+const App = () => {
+  return (
+  <div>
+  <AppHeader/>
+  <SearchPanel/>
+  <ToDoList/>
+</div>
+  );
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+    
