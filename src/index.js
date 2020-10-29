c46d45a826fa2c7be8087cda805779139d8c5757
Dzmitry Slepiankou <d.slepiankou@godeltech.com>
2020-10-29 12:05:14 +0300

    sort files to folders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import AppHeader from "./components/app-header";
 
 ReactDOM.render(<App />,
   document.getElementById('root'));
